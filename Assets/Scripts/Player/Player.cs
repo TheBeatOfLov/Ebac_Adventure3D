73cdb1ebcc32b2ab8c95232f0185ecdadf868a2d
TheBeatOfLov <bomdia2302@outlook.com>
2023-07-16 15:35:49 -0300

    Added run

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Animation")]
+    public Animator animator;
+
+    [Header("Movement")]
+    public CharacterController characterController;
     public float speed = 5f;
     public float turnSpeed = 1f;
     public float gravity = 9.8f;
-    public CharacterController characterController;
+    public float jumpSpeed = 5f;
+
+    [Header("Running Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float runSpeed = 1.5f;
 
     private float _vSpeed = 0f;
 
@@ -19,20 +28,54 @@ public class Player : MonoBehaviour
     private void Movement()
     {
         //Implementando movimento Pra frente e pros lados
-        var speedVectorVert = transform.forward * Input.GetAxis("Vertical") * speed;
-        var speedVectorHor = transform.right * Input.GetAxis("Horizontal") * speed;
+        var inputAxisVert = Input.GetAxis("Vertical");
+        var inputAxisHor = Input.GetAxis("Horizontal");
+
+        var speedVectorVert = transform.forward * inputAxisVert * speed;
+        var speedVectorHor = transform.right * inputAxisHor * speed;
+
+        var isWalking = inputAxisVert != 0 || inputAxisHor != 0;
 
         //gravidade
         _vSpeed -= gravity * Time.deltaTime;
         speedVectorVert.y = _vSpeed;
         speedVectorHor.y = _vSpeed;
 
+        //animações movement
 
+        if (isWalking)
+        {
+            if (Input.GetKey(keyRun))
+            {
+                speedVectorVert *= runSpeed;
+                speedVectorHor *= runSpeed;
+                animator.speed = runSpeed;
+            }
+            animator.SetBool("Run", true);
+        }
+
+        else
+        {
+            animator.SetBool("Run", false);
+            animator.speed = 1;
+        }
+
+        //comando Move
         characterController.Move(speedVectorVert * Time.deltaTime);
         characterController.Move(speedVectorHor * Time.deltaTime);
 
         /*O prof fez assim (por rotação), mas parece um carrinho entao não curti:
-        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);*/
+        transform.Rotate(0, inputAxisHor * turnSpeed * Time.deltaTime, 0);*/
+
+        //jump
+        if (characterController.isGrounded)
+        {
+            _vSpeed = 0f;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _vSpeed = jumpSpeed;
+            }
+        }
 
     }
 }
